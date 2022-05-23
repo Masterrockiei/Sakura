af5783a8c4af4f1c0b4d5041580c43cac3c30ae5
Amal nath <96761219+Masterrockiei@users.noreply.github.com>
2022-05-23 16:55:14 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -75,7 +75,7 @@ def a(client, message: Message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🏷 <b>Title:</b> <a href="{link}">{title}</a>⏳ <b>Duration:</b> <code>{duration}</code>\n👀 <b>Views:</b> <code>{views}</code>\n🎧 <b>Requested By:</b> {message.from_user.mention()} \n📤 <b>Uploaded By: @ElsamovienewBot</b>'
+        rep = f'🏷 <b>Title:</b> <a href="{link}">{title}</a>⏳ <b>Duration:</b> <code>{duration}</code>\n👀 <b>Views:</b> <code>{views}</code>\n🎧 <b>Requested By:</b> {message.from_user.mention()} \n'
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
