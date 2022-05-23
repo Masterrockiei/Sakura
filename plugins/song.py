d70e4d3d47088daacc44ace20fc7a624d14edf61
Amal nath <96761219+Masterrockiei@users.noreply.github.com>
2022-05-23 16:45:35 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -54,7 +54,7 @@ def a(client, message: Message):
             #     m.edit("Exceeded 30mins cap")
             #     return
             
-            performer = f"[Sakura]" 
+            performer = f"[Sakura music]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
