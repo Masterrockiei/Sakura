b79c2c55495f791bea4bb68bc49cd2e927fe432e
Amal nath <96761219+Masterrockiei@users.noreply.github.com>
2022-05-24 15:14:21 +0530

    Update log_channel.py

--- plugins/log_channel.py
+++ plugins/log_channel.py
@@ -48,3 +48,57 @@ if is_module_loaded(FILENAME):
             result = func(bot, update, *args, **kwargs)
             chat = update.effective_chat
             message = update.effective_message
+            
+            if result:
+                datetime_fmt = "%H:%M - %d-%m-%Y"
+                result += "\n<b>Event Stamp</b>: <code>{}</code>".format(datetime.utcnow().strftime(datetime_fmt))
+
+                if message.chat.type == chat.SUPERGROUP and message.chat.username:
+                    result += f'\n<b>Link:</b> <a href="https://t.me/{chat.username}/{message.message_id}">click here</a>'
+                log_chat = str(GBAN_LOGS)
+                if log_chat:
+                    send_log(bot, log_chat, chat.id, result)
+            elif result != "":
+                LOGGER.warning("%s was set as loggable to gbanlogs, but had no return statement.", func)
+
+            return result
+
+        return glog_action
+
+
+    def send_log(bot: Bot, log_chat_id: str, orig_chat_id: str, result: str):
+
+        try:
+            bot.send_message(log_chat_id, result, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+        except BadRequest as excp:
+            if excp.message == "Chat not found":
+                bot.send_message(orig_chat_id, "This log channel has been deleted - unsetting.")
+                sql.stop_chat_logging(orig_chat_id)
+            else:
+                LOGGER.warning(excp.message)
+                LOGGER.warning(result)
+                LOGGER.exception("Could not parse")
+
+                bot.send_message(log_chat_id, result + "\n\nFormatting has been disabled due to an unexpected error.")
+
+
+    @run_async
+    @user_admin
+    def logging(bot: Bot, update: Update):
+
+        message = update.effective_message
+        chat = update.effective_chat
+
+        log_channel = sql.get_chat_log_channel(chat.id)
+        if log_channel:
+            log_channel_info = bot.get_chat(log_channel)
+            message.reply_text(f"This group has all it's logs sent to:"
+                               f" {escape_markdown(log_channel_info.title)} (`{log_channel}`)",
+                               parse_mode=ParseMode.MARKDOWN)
+
+        else:
+            message.reply_text("No log channel has been set for this group!")
+            
+            
+            
+            
