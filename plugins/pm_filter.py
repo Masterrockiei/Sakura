543af894b86f6cd948973487203897c52ab86539
Amal nath <96761219+Masterrockiei@users.noreply.github.com>
2022-06-29 15:22:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -73,7 +73,7 @@ async def next_page(bot, query):
                     text=f"✨{file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
+                    text=f"✨{get_size(file.file_size)}",
                     callback_data=f'files_#{file.file_id}',
                 ),
             ]
